e9fa456c2abff0d006fe969635299a651024a419
Howard Li <bighorn@pursuitofchallenge.com>
2019-12-24 16:23:13 -0800

    Add questions regarding project type, submodules and header placement

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,13 +4,61 @@ const assert = require("yeoman-assert");
 const helpers = require("yeoman-test");
 
 describe("generator-pitchfork:app", () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({ someAnswer: true });
+  let runContext;
+
+  beforeEach(() => {
+    runContext = helpers.run(path.join(__dirname, "../generators/app"));
   });
 
-  it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
+  describe("Submodules", () => {
+    it("creates a library project without submodules", async () => {
+      await runContext.withPrompts({
+        projectType: "Library Project",
+        usingSubmodules: false
+      });
+
+      assert.file("src");
+    });
+
+    it("creates a library project with submodules", async () => {
+      await runContext.withPrompts({
+        projectType: "Library Project",
+        usingSubmodules: true
+      });
+
+      assert.file(["libs", "include"]);
+    });
+
+    it("creates an application project without submodules", async () => {
+      await runContext.withPrompts({
+        projectType: "Application Project",
+        usingSubmodules: false
+      });
+
+      assert.file("src/main.cpp");
+    });
+
+    it("creates an application project with submodules", async () => {
+      await runContext.withPrompts({
+        projectType: "Application Project",
+        usingSubmodules: true
+      });
+
+      assert.file("libs/main/src/main.cpp");
+    });
+  });
+
+  describe("Library Header Placement", () => {
+    it("creates a library project with merged header placement", async () => {
+      await runContext.withPrompts({ separateHeaders: false });
+
+      assert.noFile("include");
+    });
+
+    it("creates a library project with separated header placement", async () => {
+      await runContext.withPrompts({ separateHeaders: true });
+
+      assert.file("include");
+    });
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,36 @@
 "use strict";
 const Generator = require("yeoman-generator");
-const chalk = require("chalk");
 const yosay = require("yosay");
+const fs = require("fs");
+
+const { promisify } = require("util");
+const mkdir = promisify(fs.mkdir);
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the groundbreaking ${chalk.red(
-          "generator-pitchfork"
-        )} generator!`
-      )
-    );
+    this.log(yosay("Please answer the following questions."));
 
     const prompts = [
       {
+        type: "list",
+        name: "projectType",
+        message: "What type of project do you wish to create?",
+        choices: ["Library Project", "Application Project"],
+        default: "Library Project"
+      },
+      {
         type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
+        name: "usingSubmodules",
+        message: "Will this project be using submodules?",
+        default: false
+      },
+      {
+        type: "confirm",
+        name: "separateHeaders",
+        message:
+          "Do you want to place the public headers in a separate directory?",
+        default: false
       }
     ];
 
@@ -30,10 +41,27 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
+    if (this.props.projectType === "Library Project") {
+      if (this.props.usingSubmodules || this.props.separateHeaders) {
+        mkdir("include");
+      }
+
+      if (this.props.usingSubmodules) {
+        mkdir("libs");
+      } else {
+        mkdir("src");
+      }
+    } else {
+      let destPath;
+
+      if (this.props.usingSubmodules) {
+        destPath = this.destinationPath("libs/main/src/main.cpp");
+      } else {
+        destPath = this.destinationPath("src/main.cpp");
+      }
+
+      this.fs.copy(this.templatePath("main.cpp"), destPath);
+    }
   }
 
   install() {
