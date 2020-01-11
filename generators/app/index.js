33f015941064b1ef6c996da47cc25f7b72de398d
Howard Li <bighorn@pursuitofchallenge.com>
2020-01-11 15:29:03 -0800

    Import the license choices instead of explicitly defining them

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,20 +3,7 @@ const yosay = require("yosay");
 const PitchforkGenerator = require("../base.js");
 
 const generatorSubmodule = require("../submodule/index.js");
-
-const licenses = [
-  { name: "Apache 2.0", value: "Apache-2.0" },
-  { name: "MIT", value: "MIT" },
-  { name: "Mozilla Public License 2.0", value: "MPL-2.0" },
-  { name: "BSD 2-Clause (FreeBSD) License", value: "BSD-2-Clause-FreeBSD" },
-  { name: "BSD 3-Clause (NewBSD) License", value: "BSD-3-Clause" },
-  { name: "Internet Systems Consortium (ISC) License", value: "ISC" },
-  { name: "GNU AGPL 3.0", value: "AGPL-3.0" },
-  { name: "GNU GPL 3.0", value: "GPL-3.0" },
-  { name: "GNU LGPL 3.0", value: "LGPL-3.0" },
-  { name: "Unlicense", value: "unlicense" },
-  { name: "No License (Copyrighted)", value: "UNLICENSED" }
-];
+const { licenses } = require("generator-license");
 
 module.exports = class extends PitchforkGenerator {
   prompting() {
